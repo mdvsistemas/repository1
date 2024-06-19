e533f0eaf6b3de4979b8cf2d45524faafe41aebd
Marcio Da Veiga <142607485+mdvsistemas@users.noreply.github.com>
2024-06-19 16:50:26 -0300

    Update base.py

--- base.py
+++ base.py
@@ -81,13 +81,61 @@ class ItemAcomp(Base):
 # Conexão com o banco de dados (SQLite, por exemplo)
 engine = create_engine('sqlite:///loja.db')
 Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+def AdicionaCliente(nliente):
+   
+    session.add(nliente)
+    session.commit()
+    session.close()
+    print("Cliente adicionado com sucesso!")
 
-# Criar uma sessão
+'''# Criar uma sessão
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Adicionar dados de exemplo (se necessário)
-# ...
+# Inserir dados
+# Criar grupos
+grupo1 = Grupo(descricao="Grupo 1")
+grupo2 = Grupo(descricao="Grupo 2")
+session.add(grupo1)
+session.add(grupo2)
+session.commit()
+
+# Criar produtos
+produto1 = Produto(produto="Produto 1", descricao="Descrição do Produto 1", idgrupo=grupo1.idgrupo, preco=10.0, unidade="un")
+produto2 = Produto(produto="Produto 2", descricao="Descrição do Produto 2", idgrupo=grupo1.idgrupo, preco=15.0, unidade="un")
+produto3 = Produto(produto="Produto 3", descricao="Descrição do Produto 3", idgrupo=grupo2.idgrupo, preco=20.0, unidade="un")
+produto4 = Produto(produto="Produto 4", descricao="Descrição do Produto 4", idgrupo=grupo2.idgrupo, preco=25.0, unidade="un")
+session.add(produto1)
+session.add(produto2)
+session.add(produto3)
+session.add(produto4)
+session.commit()
+
+# Criar clientes
+cliente1 = Cliente(nome="Cliente 1", telefone="123456789", email="cliente1@example.com")
+cliente2 = Cliente(nome="Cliente 2", telefone="987654321", email="cliente2@example.com")
+session.add(cliente1)
+session.add(cliente2)
+session.commit()
+
+# Criar carrinho para cliente1
+carrinho1 = Carrinho(idcliente=cliente1.idcliente, dtcriacao="2024-06-18", totalcarrinho=0.0)
+session.add(carrinho1)
+session.commit()
+
+# Adicionar itens ao carrinho
+item1 = ItemCarrinho(idcarrinho=carrinho1.idcarrinho, quantidade=2, totalitem=produto1.preco * 2, idproduto=produto1.idproduto, observacao="Sem observação")
+item2 = ItemCarrinho(idcarrinho=carrinho1.idcarrinho, quantidade=1, totalitem=produto3.preco, idproduto=produto3.idproduto, observacao="Sem observação")
+session.add(item1)
+session.add(item2)
+session.commit()
+
+# Atualizar total do carrinho
+carrinho1.totalcarrinho = item1.totalitem + item2.totalitem
+session.commit()
 
 # Fechar a sessão
 session.close()
+'''
